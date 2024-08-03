88d02faf29ca069930d72da90e9c3d438e00145f
shlyapine <shlyapinegorka@gmail.com>
2024-08-03 12:43:31 +0300

    add: deploy forkflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/accounting_ts_react_redux/",
   server: {
     open: true,
   },
